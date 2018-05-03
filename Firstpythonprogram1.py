71868c3e7a2aafdb7dce38b0e700dfcf154a6840
tchaitanya2288 <tchaitanya.2288@gmail.com>
2018-05-03 14:00:58 -0400

    Print program

---
+++ Firstpythonprogram1.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python
+
+#! = Shebang
+# /usr/bin/python = Absolute path to the python software
+
+# Quotes '', " ", ''' ''', """ """ & # = Single Line Comments
+# Quotes ''' ''' & """ """ = Multi Line Comments
+
+
+'Creating variables in python'
+
+''' 
+A-Z
+a-z
+_
+0-9 Note: A variable name can not start with digit as prefix
+'''
+Phone = 'iphone x'
+Os_Type = "Windows"
+_model_no = 3050
+_priceValue = 50000
+_007 = "Bond"
+
+"Accessing variables in python"
+
+print(Phone)
+print(Os_Type)
+print(_model_no)
+print(_priceValue)
+print(_007)
