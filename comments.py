614b0dc76cb5d794625b40d54c5bf6d0458b44ac
tchaitanya2288 <tchaitanya.2288@gmail.com>
2018-05-03 14:07:09 -0400

    python comments

---
+++ comments.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python
+
+#! = Shebang
+#/usr/bin/python = Absolute Path
+
+# This is a single line comment
+
+# Creating variables :
+a=111
+b=222
+c=a+b
+print(c)
+
+# Below is the example of multi-line comment :
+
+''' This is 
+a multi Line
+comment '''
+
+"""This is a
+multi line
+comment"""
