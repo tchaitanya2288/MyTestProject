f021afe43919d474476141d3b6fd7d957520d3b7
tchaitanya2288 <tchaitanya.2288@gmail.com>
2018-05-04 13:54:01 -0400

    python comments

---
+++ testprogram.py
@@ -0,0 +1,10 @@
+list1 = [23,45.67,'stephen',"feb_22"]
+list2 = ['vikram',"july","7-7-2014"]
+
+tup1 = 22,"feb",1988,2009
+tup2 = 10,"dec",1983,2009
+
+print(list1,id(list1),len(list1),type(list1))
+print(list2,id(list2),len(list2),type(list2))
+print(tup1,id(tup1),len(tup1),type(tup1))
+print(tup2,id(tup2),len(tup2),type(tup2))
